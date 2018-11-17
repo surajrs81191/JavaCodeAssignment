47b4025adcb1799d5cc4151cc5e1c820677ef22f
suraj <surajrs81191@gmail.com>
2018-11-17 17:57:14 +0530

    added chicken test 2c 2d

--- src/test/java/com/animal/AnimalTest.java
+++ src/test/java/com/animal/AnimalTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.animal.animals.Chicken;
 import com.animal.animals.Duck;
+import com.animal.features.Fly;
 import com.animal.features.Swim;
 
 
@@ -19,9 +21,18 @@ public class AnimalTest {
 	public void duckTest() {
 		Bird duck = new Duck();
 		//2a
-		assertEquals("duck Sound ..",duck.sound(), "Quack, quack");
+		assertEquals("duck Sound ..", "Quack, quack",duck.sound());
 		//2b
-		assertEquals("duck can swim ?",duck instanceof Swim, true);
+		assertEquals("duck can swim ?", true,duck instanceof Swim);
+	}
+	
+	@Test
+	public void chickenTest() {
+		Bird chicken = new Chicken();
+		//2c
+		assertEquals("chicken Sound ..", "Cluk Cluk",chicken.sound());
+		//2d
+		assertEquals("duck can not Fly ?", false,chicken instanceof Fly);
 	}
 	
 }
