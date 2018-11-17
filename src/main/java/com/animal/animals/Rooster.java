cd6fd34c62bfea6059349e1f4e9b77f545b212c7
suraj <surajrs81191@gmail.com>
2018-11-17 17:57:25 +0530

    Added rooster.

---
+++ src/main/java/com/animal/animals/Rooster.java
@@ -0,0 +1,12 @@
+package com.animal.animals;
+
+import com.animal.NonFlyingBird;
+
+public class Rooster extends NonFlyingBird {
+
+	@Override
+	public String sound() {
+		return "Cock-a-doodle-doo";
+	}
+
+}
